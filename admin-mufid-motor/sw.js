c0411d8eaca6fe23f6d94752630d7dee7235c5da
Digital Technology 2045 <digitaltechnology2045@gmail.com>
2025-09-29 12:01:54 +0700

    Update sw.js

--- admin-mufid-motor/sw.js
+++ admin-mufid-motor/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'admin-mufid-motor-cache-v1';
+const CACHE_NAME = 'admin-mufid-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
@@ -28,4 +28,5 @@ self.addEventListener('fetch', event => {
         return fetch(event.request);
       })
   );
+
 });
